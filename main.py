46883fc602114b6d6107c7edcd4cd9aa93d1873f
Awwab <mawwabkhank2006@gmail.com>
2024-02-11 14:10:46 +0500

    get_internet_speed and print

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', True)
 
-PROMISED_DOWN = 16
-PROMISED_UP = 16
+PROMISED_DOWN = 20
+PROMISED_UP = 20
 TWITTER_EMAIL = 'fakeofawwab@gmail.com'
 TWITTER_PASSWORD = '&zwj2xGe)XsC,%v'
 
@@ -21,6 +22,11 @@ class InternetSpeedTwitterBot:
 
     def get_internet_speed(self):
         self.driver.get('https://www.speedtest.net/')
+        self.driver.find_element(By.CLASS_NAME, 'start-text').click()
+        time.sleep(60)
+        self.down = float(self.driver.find_element(By.CLASS_NAME, 'download-speed').text)
+        self.up = float(self.driver.find_element(By.CLASS_NAME, 'upload-speed').text)
+        print(f"Download Speed: {self.down}\nUpload Speed: {self.up}")
 
 
 
